b902d2ac01412f401fc53d9354ceac5b81d706f6
Kateryna Lutska <ket.lutska@gmail.com>
2021-03-11 14:05:09 +0200

    add localStorage

--- src/App.js
+++ src/App.js
@@ -38,6 +38,21 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   formSubmitHandler = data => {
     const { contacts } = this.state;
 
@@ -88,7 +103,7 @@ class App extends Component {
               <ContactList
                 contacts={visibleFilter}
                 onDelete={this.deleteContacts}
-              ></ContactList>
+              />
             </>
           )}
         </Container>
